caf1ae0f3513bdcf5ae54441964be01a4aa0a6f6
AMM <annemariemichaud@gmail.com>
2020-03-17 17:38:15 +0100

    style : Ajout de commentaires
    ajout de commentaires dans le code.

--- batch.py
+++ batch.py
@@ -1,33 +1,44 @@
 import openpyxl
 import unicodedata
 import re
+#fichier de données contenant la liste des personnels avec leurs numens
+#RGPD -> attention à la conservation de ce fichier 
 from data import numens as numens
 
 def nettoyer_fichier(valeur):
+    #les valeurs saisies dans le fichier liste doit être nettoyées et normalisées pour pouvoir être comparées
     if valeur is not None:
+        #encodage du fichier
         valeur=unicodedata.normalize('NFKD',valeur).encode('ASCII', 'ignore').decode('ASCII')
+        #suppression des espaces avant et après la valeur
         valeur=valeur.strip()
+        #passage de toutes les valeurs en caractères minuscules
         valeur=valeur.upper()
+        #remplacement des - en espace
         if valeur.find('-'):
             valeur=valeur.replace('-',' ')
     return valeur
 
+#ouverture du fichier liste
 wb = openpyxl.load_workbook('liste.xlsx')
-
+#référencement de l'onglet actif 
 onglet = wb.active
 
-liste_titre_colonne = []
 
+liste_titre_colonne = []
 personnes = []
 
-#ecrire les resultas dans un fichier
+#écrire les résultats dans un fichier ficCandidat.unl
 ficCandidatFile = open('ficCandidat.unl', 'w')
+#écrire les erreurs (personnes non trouvées et/ou personnes avec plusieurs identifications possibles
 erreurFile = open('erreur.txt','w')
+#écrire la liste des disciplines pour vérification (très important) pour éviter les erreurs d'identification
 verificationFile = open('verification.txt','w')
-#recuperation donnees liste à traiter
+
+#récupération des titres des colonnes du fichier liste
 for i in range(1,onglet.max_column+1):
     liste_titre_colonne.append(onglet.cell(row=1,column=i).value)
-
+#récupération de la liste des personnes à convoquer
 for i in range(2,onglet.max_row+1):
     personne = {}
     for j in range(0,onglet.max_column):
@@ -35,21 +46,24 @@ for i in range(2,onglet.max_row+1):
     personnes.append(personne)
 
 
-#nettoyer le fichier de donnees
+#nettoyer le fichier de données
 for ligne in personnes:
     for cle in ligne.keys():
         if cle in ('Nom','Prenom','Discipline','Ville','RNE'):
             ligne[cle]=nettoyer_fichier(ligne[cle])
 
+
 global trouve
 trouve=0
+#résultat si tous les champs de la liste de personnes à convoquer concordent avec la liste des numens
 resultat_fort = []
+##résultat si certains champs de la liste de personnes à convoquer concordent avec la liste des numens
 resultat_faible = []
-#chercher les personnes
 
-    
+#chercher les personnes   
 for ligne in personnes:
 
+    #selon deux listes resultats faibles ou forts
     for numen in numens:
         if (numen['Nom usage'] is not None and numen['Nom patronymique'] is not None) and numen['Prenom'] is not None:
             if (ligne['Nom'] == numen['Nom usage'] or ligne['Nom'] == numen['Nom patronymique']) and  ligne['Prenom'] == numen['Prenom']:
@@ -78,7 +92,7 @@ for ligne in personnes:
                 if len(resultat_fort)==0 and len(resultat_faible)==0:
                     resultat_faible.append(numen)
                     
-
+    #écriture des résultats dans les fichiers erreurs, vérification et ficCandidat.unl
     if len(resultat_fort)==1:
         for numen in resultat_fort:
             candidature = numen['Numen'] + '|' + str(ligne['Dispositif']) + '|' + str(ligne['Module']) + '|' + str(ligne['Groupe']).rjust(2,'0') + '|R|\n'
